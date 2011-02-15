495f760030f7d6244b8d383ece82aaeac5af152e
Espen Volden <voldern@hoeggen.net>
2011-02-15 13:05:55 +0100

    * Added 'add_sms' method to api class and removed 'create_request'.   This makes it possible to send several messages in one request.   The 'send_sms' method works as before, but the arguments are now   optional.
    * The lib won't try to print the request response when running in
      testing mode, preventing a NoMethodError exception.
    
    * Upped version to 0.1.3

--- lib/pswincom/api.rb
+++ lib/pswincom/api.rb
@@ -3,6 +3,7 @@
 ).each do |lib|
     require File.join(File.dirname(__FILE__), lib)
 end
+require 'rexml/document'
 
 module PSWinCom
   class API
@@ -16,21 +17,32 @@ module PSWinCom
     self.debug_mode = false;
 
     def initialize user, password
-      @user, @password = user, password 
+      @user, @password = user, password
+      @request = Request.new :user => @user, :passwd => @password      
     end
 
-    def send_sms to, text, args={}      
-      request = create_request to, text, args
-      sender = HttpSender.new 
-      PSWinCom.debug "Request", request.xml
-      result = sender.send(request) unless self.class.test_mode
-      PSWinCom.debug "Result", result
+    def send_sms to = nil, text = nil, args={}
+      add_sms(to, text, args) unless (to.nil? || text.nil?)
+
+      # Check if there are any messages to send
+      if REXML::Document.new(@request.xml).
+          root.get_elements('MSGLST/MSG').count == 0
+        raise ArgumentError, 'There are no SMS to send'
+      end
+      
+      sender = HttpSender.new
+      PSWinCom.debug "Request", @request.xml
+      
+      unless self.class.test_mode
+        result = sender.send(@request) 
+        PSWinCom.debug "Result", result.body
+      end
+      
       return result
     end
 
-    def create_request to, text, args
-      request = Request.new :user => @user, :passwd => @password      
-      request.add({:text => text, :receiver => to }.merge(args))
+    def add_sms to, text, args = {}
+      @request.add({:text => text, :receiver => to }.merge(args))
     end
   end
 end
--- pswincom.gemspec
+++ pswincom.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name            = 'pswincom'
-  s.version         = '0.1.2'
+  s.version         = '0.1.3'
   s.authors         = ['PSWinCom AS']
   s.email           = ['support@pswin.com']
   s.homepage        = 'http://github.com/tormaroe/pswincomgem'
